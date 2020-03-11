faacbf84f2dbc5642b30eeece169b71a27346b92
Kanoknet <61303473+Kanoknet@users.noreply.github.com>
2020-03-11 22:21:46 +0700

    Add files via upload

---
+++ web port/js6register.js
@@ -0,0 +1,24 @@
+window.onload = pageLoad;
+function pageLoad(){
+    var form = document.getElementById("myForm");
+    form.onsubmit = validateForm;
+	
+}
+
+function validateForm() {
+
+    var pass1 = document.forms["myForm"]["password1"].value;
+    var pass2 = document.forms["myForm"]["password2"].value;
+    // alert(name);
+    if (pass1 != pass2){
+        var form = document.getElementById("errormsg");
+        form.innerHTML = "password error"
+        return false;
+    
+    }else{
+        return true;
+    }
+        
+
+    //ถ้าตรวจสอบแล้วว่ามีการ register ไม่ถูกต้องให้ return false ด้วย
+}
